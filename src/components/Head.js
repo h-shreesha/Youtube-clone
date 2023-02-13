0373190d43053ce960aaba5325238552ed4c7f67
Shreesha H R <shreeshahr9303@gmail.com>
2023-02-13 20:02:06 +0530

    added files

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleMenu } from '../utils/appSlice';
 
 const Head = () => {
+
+    const dispatch = useDispatch();
+    const toggleMenuHandler = () => {
+        dispatch(toggleMenu());
+    }
+
     return (
-        <div className="flex p-5 justify-between ">
+        <div className="flex p-5 justify-between shadow-lg ">
             <div className="flex">
                 <img
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/2048px-Hamburger_icon.svg.png"
                     alt="hamburgerMenu"
-                    className="w-10 h-10"
+                    className="w-10 h-10 cursor-pointer"
+                    onClick={() => toggleMenuHandler()}
                 />
                 <img
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/YouTube_Logo_%282013-2017%29.svg/1280px-YouTube_Logo_%282013-2017%29.svg.png"
@@ -16,9 +25,13 @@ const Head = () => {
                 />
             </div>
             <div>
-                <input type="text" 
-                className='w-[35rem] border-gr'/>
-                <button>🔍 Search</button>
+                <input
+                    type="text"
+                    className="w-[35rem] outline-none rounded-l-full border border-gray-300 bg-slate-200 py-2 pl-5"
+                />
+                <button className="border border-gray-300 bg-slate-200 py-2 pl-2 pr-3 rounded-r-full  ">
+                    🔍 
+                </button>
             </div>
             <div>
                 <img
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const SideBar = () => {
-  return (
-    <div>SideBar</div>
-  )
-}
+    const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+    if(!isMenuOpen) return null;
 
-export default SideBar
+    return (
+        <div>
+            <div className="px-5 shadow-xl pt-5">
+                <ul>
+                    <li>Home</li>
+                    <li>Shorts</li>
+                    <li>Video</li>
+                    <li>Live</li>
+                </ul>
+                <h1 className="font-bold text-lg pt-5">Subscriptions</h1>
+                <ul>
+                    <li>Musics</li>
+                    <li>Sports</li>
+                    <li>Gaming</li>
+                    <li>Movies</li>
+                </ul>
+                <h1 className="font-bold text-lg pt-5">Watch Later</h1>
+                <ul>
+                    <li>Musics</li>
+                    <li>Sports</li>
+                    <li>Gaming</li>
+                    <li>Movies</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default SideBar;
