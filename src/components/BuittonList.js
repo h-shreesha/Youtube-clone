940d853cd574f85d652a0aac4195dfd3dd74d7bd
Shreesha H R <shreeshahr9303@gmail.com>
2023-02-21 08:06:06 +0530

    updated

--- src/components/BuittonList.js
+++ src/components/BuittonList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Buttons from './Buttons'
 
-const  btns = ['All', 'Gamings', 'Songs', 'Live', 'Cricket', 'Football', 'Technology', 'Science', 'Soccer', 'Javascript','React','Node']
+const  btns = ['All', 'Gamings', 'Songs', 'Live', 'Cricket', 'Football', 'Technology', 'Science', 'Soccer', 'Javascript','React','Node', 'Typescript']
 const BuittonList = () => {
   return (
     <div className='flex'>
