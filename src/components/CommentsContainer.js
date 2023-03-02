3266f68b4dcc1a49a3afe86e07799aac6a9dddec
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-02 21:45:31 +0530

    comment section completed

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -64,17 +64,18 @@ const commentsData = [
 ];
 
 const Comment = ({data}) => {
+    
     const {name, text} = data;
     return (
         <>
-            <div className='flex gap-2'>
+            <div className='px-2 flex gap-2 shadow-sm bg-gray-200 rounded-md mb-2'>
                 <img
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXQcP51A3ncwchFeonpESD-_s8Gg043M_a2g&usqp=CAU"
                     alt="cmtImg"
                     className="w-8 h-8"
                 />
-                <div>
-                    <div>{name}</div>
+                <div >
+                    <div className='font-bold'>{name}</div>
                     <div>{text}</div>
                 </div>
             </div>
@@ -82,11 +83,23 @@ const Comment = ({data}) => {
     );
 }
 
+const CommentList = ({comments}) => {
+    return comments.map((cmtdata) => {
+        return (
+            <div>
+                <Comment data={cmtdata} />
+                <div className='pl-5 border border-l-black ml-5'>
+                    <CommentList comments={cmtdata.replies} />
+                </div>
+            </div>
+        )})
+}
+
 const CommentsContainer = () => {
     return (
         <div className='px-2 py-2 ml-3'>
             <h1 className='font-bold text-2xl'>Comments : </h1>
-            <Comment data = {commentsData[0]} />
+            <CommentList comments = {commentsData} />
         </div>
     );
 };
