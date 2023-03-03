c6fc064eea6af89241636925651ae5edad1435b0
Shreesha H R <shreeshahr9303@gmail.com>
2023-03-03 20:36:31 +0530

    updated card styling

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -9,7 +9,7 @@ const VideoCard = ({ info }) => {
         <div className='px-2 mx-1 py-5 w-64 shadow-lg rounded-lg cursor-pointer hover:bg-gray-200'>
             <img src={thumbnails.medium.url} alt="" className='rounded-lg'/>
             <ul>
-                <li className='text-lg font-bold'>{title}</li>
+                <li className='text-md font-bold'>{title}</li>
                 <li>{channelTitle}</li>
                 <li>{statistics.viewCount}</li>
             </ul>
